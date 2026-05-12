68b153256e396a84a2c6d9d70271f5b4ac563f6a
Nagarjuna789 <nagarjuna5942@gmail.com>
2026-05-12 21:23:13 +0530

    initial changes

---
+++ src/main/java/dec2025/jenkins_practice_gsitdec25/App.java
@@ -0,0 +1,10 @@
+package dec2025.jenkins_practice_gsitdec25;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/dec2025/jenkins_practice_gsitdec25/LoginTest.java
@@ -0,0 +1,11 @@
+package dec2025.jenkins_practice_gsitdec25;
+
+import org.testng.annotations.*;
+
+public class LoginTest {
+	@Test
+	void hello() {
+		System.out.println("Login Test is working");
+	}
+
+}
